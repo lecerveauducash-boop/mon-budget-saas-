a3ff4d6b1a2dc2da0e56221bec6148e95ddf5bb9
lecerveauducash-boop <lecerveauducash@gmail.com>
2026-08-14 20:51:30 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,11 @@ export default function Home() {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          if (entry.isIntersecting) entry.target.classList.add('visible');
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+          } else {
+            entry.target.classList.remove('visible');
+          }
         });
       },
       { threshold: 0.2 }
